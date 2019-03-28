5f2e942ee2348ded819a176a49cc39371f765df7
shrn82256 <shrn82256@gmail.com>
2019-03-28 22:39:11 +0530

    background reload fix

--- task-manager.py
+++ task-manager.py
@@ -4,38 +4,44 @@ import processes
 import json
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
+
 
 @app.route("/filesystem")
 def filesystem():
-	return render_template("fs.html")
+    return render_template("fs.html")
+
 
 @app.route("/resources")
 def resources():
-	return render_template("res.html")
+    return render_template("res.html")
+
 
 @app.route('/processes', methods=['POST'])
 def getProcessesData():
-	return json.dumps(processes.getAllPInfo())
+    return json.dumps(processes.getAllPInfo())
+
 
 @app.route('/killproc', methods=['POST'])
 def killproc():
-	pid = int(request.form['pid'])
-	processes.killProcess(pid)
-	return ""
+    pid = int(request.form['pid'])
+    processes.killProcess(pid)
+    return ""
+
 
 @app.route('/fsdata', methods=['POST'])
 def fsdata():
-	return json.dumps(processes.getAllFileSystemsInfo())
+    return json.dumps(processes.getAllFileSystemsInfo())
+
 
 @app.route('/cpugraphdata', methods=['POST'])
 def cpugraphdata():
-	return json.dumps(processes.getCPUperc())
-
+    return json.dumps(processes.getCPUperc())
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
-	# app.run(debug = True)
+    # app.run()
+    app.run(debug=True)
